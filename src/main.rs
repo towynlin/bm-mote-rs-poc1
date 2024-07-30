6bb915df9069d6a9a56cb1a1c294499ffc1bb651
Zachary Crockett <zachary.crockett@sofarocean.com>
2024-07-30 16:40:26 -0700

    Added ADIN driver

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,36 @@
 use defmt::{panic, *};
 use defmt_rtt as _; // global logger
 use embassy_executor::Spawner;
-use embassy_futures::join::join;
+use embassy_futures::join::join4;
+use embassy_net::{Ipv6Address, Ipv6Cidr, Stack, StackResources, StaticConfigV6};
+use embassy_net_adin1110::{Device, ADIN1110};
+use embassy_stm32::gpio::{Level, Output, Pull, Speed};
+use embassy_stm32::mode::Async;
+use embassy_stm32::rng::{self, Rng};
+use embassy_stm32::spi::{Config as SPI_Config, Spi};
+use embassy_stm32::time::Hertz;
 use embassy_stm32::usb::{Driver, Instance};
-use embassy_stm32::{bind_interrupts, peripherals, usb, Config};
+use embassy_stm32::{bind_interrupts, exti, peripherals, usb, Config};
+use embassy_time::Delay;
 use embassy_usb::class::cdc_acm::{CdcAcmClass, State};
 use embassy_usb::driver::EndpointError;
 use embassy_usb::Builder;
+use embedded_hal_bus::spi::ExclusiveDevice;
+use heapless::Vec;
 use panic_probe as _;
+use rand_core::RngCore;
+use static_cell::StaticCell;
 
 bind_interrupts!(struct Irqs {
     OTG_FS => usb::InterruptHandler<peripherals::USB_OTG_FS>;
+    RNG => rng::InterruptHandler<peripherals::RNG>;
 });
+
+pub type SpeSpi = Spi<'static, Async>;
+pub type SpeSpiCs = ExclusiveDevice<SpeSpi, Output<'static>, Delay>;
+pub type SpeInt = exti::ExtiInput<'static>;
+pub type SpeRst = Output<'static>;
+pub type Adin1110T = ADIN1110<SpeSpiCs>;
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
@@ -108,9 +127,65 @@ async fn main(_spawner: Spawner) {
         }
     };
 
+    const MAC: [u8; 6] = [0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff];
+    const IP_ADDRESS: Ipv6Cidr = Ipv6Cidr::new(
+        Ipv6Address([
+            0xfe, 0x80, 0, 0, 0, 0, 0, 0, 0xc0, 0xff, 0xee, 0xee, 0xf0, 0xca, 0xcc, 0x1a,
+        ]),
+        64,
+    );
+    static STATE: StaticCell<embassy_net_adin1110::State<8, 8>> = StaticCell::new();
+    let state = STATE.init(embassy_net_adin1110::State::<8, 8>::new());
+
+    let spe_spi_cs_n = Output::new(p.PA15, Level::High, Speed::High);
+    let spe_spi_sclk = p.PB3;
+    let spe_spi_miso = p.PB4;
+    let spe_spi_mosi = p.PB5;
+
+    // Don't turn the clock to high, clock must fit within the system clock as we get a runtime panic.
+    let mut spi_config = SPI_Config::default();
+    spi_config.frequency = Hertz(160_000_000);
+
+    let spe_spi: SpeSpi = Spi::new(
+        p.SPI3,
+        spe_spi_sclk,
+        spe_spi_mosi,
+        spe_spi_miso,
+        p.GPDMA1_CH13,
+        p.GPDMA1_CH12,
+        spi_config,
+    );
+    let spe_spi = SpeSpiCs::new(spe_spi, spe_spi_cs_n, Delay).unwrap();
+
+    let spe_int = exti::ExtiInput::new(p.PB8, p.EXTI8, Pull::None);
+
+    let spe_reset_n = Output::new(p.PA0, Level::Low, Speed::Low);
+
+    let (device, runner) =
+        embassy_net_adin1110::new(MAC, state, spe_spi, spe_int, spe_reset_n, true, false).await;
+    let eth_fut = runner.run();
+
+    let mut r = Rng::new(p.RNG, Irqs);
+    let seed = r.next_u64();
+    let ip_cfg = embassy_net::Config::ipv6_static(StaticConfigV6 {
+        address: IP_ADDRESS,
+        gateway: None,
+        dns_servers: Vec::new(),
+    });
+
+    static STACK: StaticCell<Stack<Device<'static>>> = StaticCell::new();
+    static RESOURCES: StaticCell<StackResources<2>> = StaticCell::new();
+    let stack = &*STACK.init(Stack::new(
+        device,
+        ip_cfg,
+        RESOURCES.init(StackResources::<2>::new()),
+        seed,
+    ));
+    let net_fut = stack.run();
+
     // Run everything concurrently.
     // If we had made everything `'static` above instead, we could do this using separate tasks instead.
-    join(usb_fut, echo_fut).await;
+    join4(usb_fut, echo_fut, eth_fut, net_fut).await;
 }
 
 struct Disconnected {}
